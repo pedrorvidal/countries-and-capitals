f5c7c195bf94d97b8d1196e6c55f60165a79f381
Pedro Vidal <pedrorvidal@gmail.com>
2025-04-21 16:49:46 -0300

    data preparing

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    private $_app_data;
+
+    public function __construct()
+    {
+        // load app_data.php file from app folder
+        $this->_app_data = require(app_path('app_data.php'));
+    }
+    public function show_data()
+    {
+        return response()->json($this->_app_data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo "hello world";
 });
+
+Route::get('/show_data', [MainController::class, 'show_data']);
